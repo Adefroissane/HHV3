44f7c0c8618e31900560c230386322be01b53ec2
Adefroissane <adefroissane@hotmail.fr>
2019-05-18 16:57:53 +0200

    "mise a jour defi"

--- src/test/java/automatisation.java
+++ src/test/java/automatisation.java
@@ -1,5 +1,6 @@
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
