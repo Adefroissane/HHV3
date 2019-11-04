efa538fa5e96316b4535eb6bf5201b2925b42ce7
Adefroissane <adefroissane@hotmail.fr>
2019-11-04 19:31:55 +0100

    "mise a jour champion"

--- src/test/java/automatisation.java
+++ src/test/java/automatisation.java
@@ -156,7 +156,7 @@ public class automatisation {
             Combat(driver);
         }
     }
-
+    @Ignore
     @Test
     public void automatisationChampion() {
         InitialisationPage initialisation = new InitialisationPage(driver);
