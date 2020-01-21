2d20b3fd6a89cd7e519fe7b9d4a6d6543e8227e3
Adefroissane <adefroissane@hotmail.fr>
2020-01-21 08:40:03 +0100

    "mise a jour champion"

--- src/main/java/ChampionPage.java
+++ src/main/java/ChampionPage.java
@@ -61,7 +61,7 @@ public class ChampionPage extends HentaiHeroesPage {
 
     public boolean championDispo(WebDriver driver) {
         WebDriverWait webDriverWait = new WebDriverWait(driver, 5);
-        webDriverWait.until(ExpectedConditions.visibilityOf(jardinAuDela));
+        webDriverWait.until(ExpectedConditions.visibilityOf(universiteElite));
         try{
             Thread.sleep(750);
         }
@@ -69,7 +69,7 @@ public class ChampionPage extends HentaiHeroesPage {
             e.printStackTrace();
         }
         try {
-            driver.findElement(By.cssSelector("#contains_all > section > a:nth-child(4) > div.champion-lair-name.map-label-link > div"));
+            driver.findElement(By.cssSelector("##contains_all > section > a:nth-child(5) > div.champion-lair-name.map-label-link > div"));
         } catch (NoSuchElementException e)
         {
             return true;
@@ -80,8 +80,8 @@ public class ChampionPage extends HentaiHeroesPage {
     public void combattreChampion(WebDriver driver){
 
         WebDriverWait webDriverWait = new WebDriverWait(driver, 5);
-        webDriverWait.until(ExpectedConditions.visibilityOf(jardinAuDela));
-        jardinAuDela.click();
+        webDriverWait.until(ExpectedConditions.visibilityOf(universiteElite));
+        universiteElite.click();
         WebDriverWait webDriverWait2 = new WebDriverWait(driver, 5);
         webDriverWait2.until(ExpectedConditions.visibilityOf(affronter));
         affronter.click();
