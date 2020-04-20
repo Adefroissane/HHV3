8a7b7495b713976a6b7d0601d8490b1ff0b8f31c
adefr <adefroissane@hotmail.fr>
2020-04-20 09:25:44 +0200

    "mise a jour event"

--- src/main/java/LastZonePage.java
+++ src/main/java/LastZonePage.java
@@ -53,8 +53,8 @@ public class LastZonePage extends HentaiHeroesPage {
     public CombatPage openCombatPage(WebDriver driver)
     {
         WebDriverWait webDriverWait = new WebDriverWait(driver, 10);
-        webDriverWait.until(ExpectedConditions.visibilityOf(affronterRoko));
-        affronterRoko.click();
+        webDriverWait.until(ExpectedConditions.visibilityOf(affronterJackson));
+        affronterJackson.click();
         return new CombatPage(driver);
 
     }
