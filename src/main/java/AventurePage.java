a130be5aa53b8d6928f7e42d96e166e782f4dc06
adefr <adefroissane@hotmail.fr>
2020-07-23 12:12:11 +0200

    "mise a jour event"

--- src/main/java/AventurePage.java
+++ src/main/java/AventurePage.java
@@ -66,8 +66,8 @@ public class AventurePage extends HentaiHeroesPage {
         }*/
 
         WebDriverWait webDriverWait2 = new WebDriverWait(driver, 5);
-        webDriverWait2.until(ExpectedConditions.visibilityOf(carole));
-        carole.click();
+        webDriverWait2.until(ExpectedConditions.visibilityOf(silvanus));
+        silvanus.click();
         return new LastZonePage(driver);
 
     }
