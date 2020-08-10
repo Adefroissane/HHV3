b238a8c74ba40ff666159a3f0fed98635ebd4786
adefr <adefroissane@hotmail.fr>
2020-08-10 19:53:54 +0200

    "mise a jour champion"

--- src/test/java/automatisation.java
+++ src/test/java/automatisation.java
@@ -168,7 +168,7 @@ public class automatisation {
         }
     }
 
-    @Ignore
+
     @Test
     public void automatisationChampion() {
         InitialisationPage initialisation = new InitialisationPage(driver);
