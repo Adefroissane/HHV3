1c6c9ba9c114ea8ae0ee5b4fd9897dd96f6c72cb
adefr <adefroissane@hotmail.fr>
2020-09-24 00:25:23 +0200

    "mise a jour bug"

--- src/main/java/CombatPage.java
+++ src/main/java/CombatPage.java
@@ -25,7 +25,7 @@ public class CombatPage extends HentaiHeroesPage{
     @FindBy(xpath = "//*[@id=\"battle_middle\"]/button[1]")
     private WebElement affronter;
 
-    @FindBy(xpath = "//*[@id=\"battle_middle\"]/div[3]/button[3]")
+    @FindBy(xpath = "//*[@id=\"battle_middle\"]/div[3]/button[2]")
     private WebElement passer2;
 
 
