edf40e927e8f59905d100a10e1ecd727fcdde06a
adefr <adefroissane@hotmail.fr>
2020-10-06 18:32:32 +0200

    "mise a jour erreur"

--- src/main/java/LastZonePage.java
+++ src/main/java/LastZonePage.java
@@ -25,7 +25,7 @@ public class LastZonePage extends HentaiHeroesPage {
     @FindBy(xpath = "//*[@id=\"worldmap\"]/a[8]")
     private WebElement affronterDarkLord;
 
-    @FindBy(xpath = "//*[@id=\"worldmap\"]/a[8]")
+    @FindBy(xpath = "//*[@id=\"worldmap\"]/a[7]")
     private WebElement affronterNinja;
 
     @FindBy(xpath = "//*[@id=\"worldmap\"]/a[10]")
