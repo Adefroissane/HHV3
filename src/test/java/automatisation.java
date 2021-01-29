a02d78572616ec63b78327c778b010fb373315af
adefr <adefroissane@hotmail.fr>
2021-01-29 13:16:14 +0100

    "mise a jour event"

--- src/test/java/automatisation.java
+++ src/test/java/automatisation.java
@@ -155,7 +155,7 @@ public class automatisation {
         marchePage.LancerAchatCadeau(driver);
     }
 
-    @Ignore
+
     @Test
     public void automatisationChampion() {
         InitialisationPage initialisation = new InitialisationPage(driver);
@@ -167,7 +167,7 @@ public class automatisation {
         }
     }
 
-    @Ignore
+
     @Test
     public void automatisationChampion2() {
         InitialisationPage initialisation = new InitialisationPage(driver);
@@ -179,7 +179,7 @@ public class automatisation {
         }
     }
 
-    @Ignore
+
     @Test
     public void automatisationChampion3() {
         InitialisationPage initialisation = new InitialisationPage(driver);
@@ -191,7 +191,7 @@ public class automatisation {
         }
     }
 
-    @Ignore
+
     @Test
     public void automatisationChampion4() {
         InitialisationPage initialisation = new InitialisationPage(driver);
@@ -203,7 +203,7 @@ public class automatisation {
         }
     }
 
-    @Ignore
+
     @Test
     public void automatisationChampion5() {
         InitialisationPage initialisation = new InitialisationPage(driver);
@@ -215,7 +215,7 @@ public class automatisation {
         }
     }
 
-    @Ignore
+
     @Test
     public void automatisationChampion6() {
         InitialisationPage initialisation = new InitialisationPage(driver);
