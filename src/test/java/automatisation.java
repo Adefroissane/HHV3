1b8543fc3584d87414fd03d023ed299b56bd6735
adefr <adefroissane@hotmail.fr>
2021-02-25 18:38:21 +0100

    "mise a jour champion club"

--- src/test/java/automatisation.java
+++ src/test/java/automatisation.java
@@ -154,7 +154,17 @@ public class automatisation {
         marchePage.LancerAchatCadeau(driver);
     }
 
-
+    @Test
+    public void automatisationChampionClub() {
+        InitialisationPage initialisation = new InitialisationPage(driver);
+        HomePage homePage = initialisation.openHomePage(driver);
+        ClubPage clubPage = homePage.openClubPage(driver);
+        clubPage.pageBoss(driver);
+        boolean bossDispo = clubPage.bossDispo(driver);
+        if (bossDispo == true) {
+            clubPage.vaincreBossDeClub(driver);
+        }
+    }
 }
 
 
