8201598b2151e157275215a2a3b830f6f7def33e
adefr <adefroissane@hotmail.fr>
2021-05-14 14:58:07 +0200

    "mise a jour event"

--- src/main/java/LastZonePage.java
+++ src/main/java/LastZonePage.java
@@ -58,8 +58,8 @@ public class LastZonePage extends HentaiHeroesPage {
     public CombatPage openCombatPage(WebDriver driver)
     {
         WebDriverWait webDriverWait = new WebDriverWait(driver, 10);
-        webDriverWait.until(ExpectedConditions.visibilityOf(affronterSake));
-        affronterSake.click();
+        webDriverWait.until(ExpectedConditions.visibilityOf(affronterDonatien));
+        affronterDonatien.click();
         return new CombatPage(driver);
 
     }
