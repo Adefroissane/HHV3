430fadbb8fed8ea79f8c6b0f7d8781623ed41b7e
adefr <adefroissane@hotmail.fr>
2021-06-04 18:30:37 +0200

    "mise a jour correction duel"

--- src/main/java/TourDeLaGloirePage.java
+++ src/main/java/TourDeLaGloirePage.java
@@ -18,13 +18,10 @@ public class TourDeLaGloirePage extends HentaiHeroesPage {
         PageFactory.initElements(driver, this);
     }
 
-    @FindBy(className = "blue_button_L")
-    private WebElement defi;
-
-    @FindBy(xpath = "//*[@id=\"battle_middle\"]/div[3]/button[1]")
+    @FindBy(xpath = "//*[@id=\"battle_middle\"]/div[2]/button[1]")
     private WebElement affronter;
 
-    @FindBy(xpath = "//*[@id=\"battle_middle\"]/div[3]/button[2]")
+    @FindBy(xpath = "//*[@id=\"battle_middle\"]/div[2]/button[2]")
     private WebElement passer;
 
     @FindBy(xpath = "//*[@id=\"rewards_popup\"]/div/button")
@@ -57,74 +54,67 @@ public class TourDeLaGloirePage extends HentaiHeroesPage {
         }
     }
 
-    public void lancerDefi(WebDriver driver)
-    {
+    public void lancerDefi(WebDriver driver) {
         try {
             recup.click();
-            ok2.click();;
-        } catch (NoSuchElementException e)
-        {
+            ok2.click();
+            ;
+        } catch (NoSuchElementException e) {
         }
         boolean defiAFaire = defiNecessaire(driver);
-        if (defiAFaire == true)
-        {
-        try {
-            Thread.sleep(750);
-        } catch (InterruptedException ab) {
-            ab.printStackTrace();
-        }
+        if (defiAFaire == true) {
+            try {
+                Thread.sleep(750);
+            } catch (InterruptedException ab) {
+                ab.printStackTrace();
+            }
 
-        List<WebElement> myElements = driver.findElements(By.xpath("//*[@id=\"leagues_middle\"]/div[3]/div[3]/table/tbody/tr[*]/td[4]"));
-        System.out.println("nbre ennemis =" + myElements.size());
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        ArrayList combats = new ArrayList<WebElement>();
-        for (WebElement e : myElements) {
-            js.executeScript("arguments[0].scrollIntoView();", e);
+            List<WebElement> myElements = driver.findElements(By.xpath("//*[@id=\"leagues_middle\"]/div[3]/div[3]/table/tbody/tr[*]/td[4]"));
+            System.out.println("nbre ennemis =" + myElements.size());
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            ArrayList combats = new ArrayList<WebElement>();
+            for (WebElement e : myElements) {
+                js.executeScript("arguments[0].scrollIntoView();", e);
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException ab) {
+                    ab.printStackTrace();
+                }
+                System.out.println(e.getText());
+                if (e.getText().equals("0/3") || e.getText().equals("1/3") || e.getText().equals("2/3")) {
+                    combats.add(e);
+                }
+            }
+            int i = combats.size();
+
+            int b = i - 1; //-1
+            WebElement adversaire = (WebElement) combats.get(b);
+            js.executeScript("arguments[0].scrollIntoView();", adversaire);
+            WebDriverWait webDriverWait = new WebDriverWait(driver, 5);
+            webDriverWait.until(ExpectedConditions.visibilityOf(adversaire));
+            adversaire.click();
             try {
                 Thread.sleep(100);
             } catch (InterruptedException ab) {
                 ab.printStackTrace();
             }
-            System.out.println(e.getText());
-            if (e.getText().equals("0/3") || e.getText().equals("1/3") ||e.getText().equals("2/3"))
-            {
-                combats.add(e);
+            driver.findElement(By.className("blue_button_L")).click();
+            WebDriverWait webDriverWait3 = new WebDriverWait(driver, 15);
+            webDriverWait3.until(ExpectedConditions.visibilityOf(affronter));
+            affronter.click();
+            WebDriverWait webDriverWait4 = new WebDriverWait(driver, 5);
+            webDriverWait4.until(ExpectedConditions.visibilityOf(passer));
+            passer.click();
+            WebDriverWait webDriverWait5 = new WebDriverWait(driver, 5);
+            webDriverWait5.until(ExpectedConditions.visibilityOf(ok));
+            try {
+                Thread.sleep(750);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
+            ok.click();
+            lancerDefi(driver);
         }
-        int i = combats.size();
-
-        int b = i-1; //-1
-        WebElement adversaire = (WebElement) combats.get(b);
-        js.executeScript("arguments[0].scrollIntoView();", adversaire);
-        WebDriverWait webDriverWait = new WebDriverWait(driver, 5);
-        webDriverWait.until(ExpectedConditions.visibilityOf(adversaire));
-        adversaire.click();
-        try {
-            Thread.sleep(100);
-        } catch (InterruptedException ab) {
-            ab.printStackTrace();
-        }
-        WebDriverWait webDriverWait2 = new WebDriverWait(driver, 5);
-        webDriverWait2.until(ExpectedConditions.visibilityOf(defi));
-        defi.click();
-        WebDriverWait webDriverWait3 = new WebDriverWait(driver, 5);
-        webDriverWait3.until(ExpectedConditions.visibilityOf(affronter));
-        affronter.click();
-        WebDriverWait webDriverWait4 = new WebDriverWait(driver, 5);
-        webDriverWait4.until(ExpectedConditions.visibilityOf(passer));
-        passer.click();
-        WebDriverWait webDriverWait5 = new WebDriverWait(driver, 5);
-        webDriverWait5.until(ExpectedConditions.visibilityOf(ok));
-        try {
-            Thread.sleep(750);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        ok.click();
-        lancerDefi(driver);
-        }
-        /*WebDriverWait webDriverWait6 = new WebDriverWait(driver, 30);
-        webDriverWait6.until(ExpectedConditions.visibilityOf(retour));
-        retour.click();*/
     }
 }
+
