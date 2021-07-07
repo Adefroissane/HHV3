522cc44c069f3fdb36a2b46323f82e93dd476e1e
adefr <adefroissane@hotmail.fr>
2021-07-07 08:40:30 +0200

    "mise a jour fille 1 etoile"

--- src/main/java/LastZonePage.java
+++ src/main/java/LastZonePage.java
@@ -58,8 +58,8 @@ public class LastZonePage extends HentaiHeroesPage {
     public CombatPage openCombatPage(WebDriver driver)
     {
         WebDriverWait webDriverWait = new WebDriverWait(driver, 10);
-        webDriverWait.until(ExpectedConditions.visibilityOf(affronterRoko));
-        affronterRoko.click();
+        webDriverWait.until(ExpectedConditions.visibilityOf(affronterDarkLord));
+        affronterDarkLord.click();
         return new CombatPage(driver);
 
     }
