07b8ea6fc0b9c4c19d21c997c0faaac3d4ee4d8c
adefr <adefroissane@hotmail.fr>
2021-07-23 21:23:56 +0200

    "mise a jour bug"

--- src/main/java/CombatPage.java
+++ src/main/java/CombatPage.java
@@ -51,9 +51,9 @@ public class CombatPage extends HentaiHeroesPage{
         WebDriverWait webDriverWait = new WebDriverWait(driver, 5);
         webDriverWait.until(ExpectedConditions.visibilityOf(affronterx1));
         affronterx1.click();
-        WebDriverWait webDriverWait2 = new WebDriverWait(driver, 30);
+        /*WebDriverWait webDriverWait2 = new WebDriverWait(driver, 30);
         webDriverWait2.until(ExpectedConditions.visibilityOf(passer));
-        passer.click();
+        passer.click();*/
         WebDriverWait webDriverWait3 = new WebDriverWait(driver, 30);
         webDriverWait3.until(ExpectedConditions.visibilityOf(oki));
 
