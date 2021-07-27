4ea8c2d019dcf0967afc25b871c43c62fe0138f0
adefr <adefroissane@hotmail.fr>
2021-07-27 12:33:56 +0200

    "mise a jour event"

--- src/test/java/automatisation.java
+++ src/test/java/automatisation.java
@@ -115,7 +115,7 @@ public class automatisation {
         }
     }
 
-
+    @Ignore
     @Test
     public void automatisationChampion() {
         InitialisationPage initialisation = new InitialisationPage(driver);
