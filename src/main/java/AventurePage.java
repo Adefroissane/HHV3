53bf48081b34a8654d0166fb3a550845e658b148
adefr <adefroissane@hotmail.fr>
2021-09-14 19:28:50 +0200

    "mise a jour event"

--- src/main/java/AventurePage.java
+++ src/main/java/AventurePage.java
@@ -60,7 +60,7 @@ public class AventurePage extends HentaiHeroesPage {
 
     public LastZonePage openLastZone(WebDriver driver) {
 
-        WebDriverWait webDriverWait = new WebDriverWait(driver, 5);
+        /*WebDriverWait webDriverWait = new WebDriverWait(driver, 5);
         webDriverWait.until(ExpectedConditions.visibilityOf(precedent));
         precedent.click();
 
@@ -69,7 +69,7 @@ public class AventurePage extends HentaiHeroesPage {
         }
         catch(InterruptedException ae){
             ae.printStackTrace();
-        }
+        }*/
 
         WebDriverWait webDriverWait2 = new WebDriverWait(driver, 5);
         webDriverWait2.until(ExpectedConditions.visibilityOf(precedent));
@@ -83,8 +83,8 @@ public class AventurePage extends HentaiHeroesPage {
         }
 
         WebDriverWait webDriverWait3 = new WebDriverWait(driver, 5);
-        webDriverWait3.until(ExpectedConditions.visibilityOf(ninja));
-        ninja.click();
+        webDriverWait3.until(ExpectedConditions.visibilityOf(finalmecia));
+        finalmecia.click();
         return new LastZonePage(driver);
 
     }
