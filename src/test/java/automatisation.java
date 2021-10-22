0c201009ebedf8442bfc6c65a654f59e6e3317e8
adefr <adefroissane@hotmail.fr>
2021-10-22 08:54:57 +0200

    "mise a jour lancement escalier"

--- src/test/java/automatisation.java
+++ src/test/java/automatisation.java
@@ -147,7 +147,7 @@ public class automatisation {
         }
     }
 
-    @Ignore
+
     @Test
     public void automatisationEscaliers() {
         InitialisationPage initialisation = new InitialisationPage(driver);
