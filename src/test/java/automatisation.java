7375f204d5e84fd55dd0eee69b00a7337ac11ab5
adefr <adefroissane@hotmail.fr>
2021-11-05 09:34:03 +0100

    "mise a jour champion"

--- src/test/java/automatisation.java
+++ src/test/java/automatisation.java
@@ -138,15 +138,15 @@ public class automatisation {
         InitialisationPage initialisation = new InitialisationPage(driver);
         HomePage homePage = initialisation.openHomePage(driver);
         ChampionPage championPage = homePage.ouvrirZoneDesChampions(driver);
-        /*boolean championDispo = championPage.championDispo(driver);
+        boolean championDispo = championPage.championDispo(driver);
         if (championDispo == true) {
             championPage.combattreChampion(driver);
-        }*/
-        boolean championDispo2 = championPage.championDispo2(driver);
+        }
+        /*boolean championDispo2 = championPage.championDispo2(driver);
         if (championDispo2 == true) {
             championPage.combattreChampion2(driver);
         }
-        /*boolean championDispo3 = championPage.championDispo3(driver);
+        boolean championDispo3 = championPage.championDispo3(driver);
         if (championDispo3 == true) {
             championPage.combattreChampion3(driver);
         }
