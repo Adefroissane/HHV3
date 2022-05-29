813cbeb051efcf101ed9864dd1d15141cb627d7c
jerome.plard <jerome.plard@groupe-nehs.com>
2022-05-29 20:41:53 +0200

    "mise a jour champion"

--- src/test/java/automatisation.java
+++ src/test/java/automatisation.java
@@ -139,7 +139,7 @@ public class automatisation {
         HomePage homePage = initialisation.openHomePage(driver);
 
         ChampionPage championPage = homePage.ouvrirZoneDesChampions(driver);
-        /*boolean championDispo = championPage.championDispo(driver);
+        boolean championDispo = championPage.championDispo(driver);
         if (championDispo == true) {
             championPage.combattreChampion(driver);
         }
@@ -162,7 +162,7 @@ public class automatisation {
         boolean championDispo6 = championPage.championDispo6(driver);
         if (championDispo6 == true) {
             championPage.combattreChampion6(driver);
-        }*/
+        }
         HomePage homepage2 = championPage.getHeader().openHomePage(driver);
 
         ClubPage clubPage = homePage.openClubPage(driver);
