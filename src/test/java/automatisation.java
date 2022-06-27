53f689edd4ca0ed19dfee8c5cca193093a594056
jerome.plard <jerome.plard@groupe-nehs.com>
2022-06-27 20:59:04 +0200

    "mise a jour test"

--- src/test/java/automatisation.java
+++ src/test/java/automatisation.java
@@ -32,7 +32,7 @@ public class automatisation {
         }
         driver.close();
     }
-
+    @Ignore
     @Test
     public void automatisationHaremCombat() {
         InitialisationPage initialisation = new InitialisationPage(driver);
@@ -66,7 +66,7 @@ public class automatisation {
             Combat(driver);
         }
     }
-
+    @Ignore
     @Test
     public void LieuxDePouvoirRecolt() {
         InitialisationPage initialisation = new InitialisationPage(driver);
@@ -74,7 +74,7 @@ public class automatisation {
         LieuxDePouvoirPage lieuxDePouvoirPage = homePage.openLieuxDePouvoirPage(driver);
         lieuxDePouvoirPage.RecoltMission(driver);
     }
-
+    @Ignore
     @Test
     public void LieuxDePouvoirLancement() {
         InitialisationPage initialisation = new InitialisationPage(driver);
@@ -82,7 +82,7 @@ public class automatisation {
         LieuxDePouvoirPage lieuxDePouvoirPage = homePage.openLieuxDePouvoirPage(driver);
         lieuxDePouvoirPage.LancerMission(driver);
     }
-
+    @Ignore
     @Test
     public void AutomatisationEscalierMission() {
         InitialisationPage initialisation = new InitialisationPage(driver);
